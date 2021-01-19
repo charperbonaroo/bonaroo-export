52b46eed908bf6b6cc6b56f1bf91ebacacbb1f2f
Toby Hinloopen <toby@bonaroo.nl>
2021-01-19 14:06:46 +0100

    Add PDF generator

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,75 @@
 import { IFile } from "./IFile";
 import * as XLSX from "xlsx";
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
-export function exportPdf(config: Readonly<ExportConfig>): IFile {
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
-}
+export async function exportPdf(inputConfig: Readonly<ExportConfig>): Promise<IFile> {
+  const config = parseExportConfig(inputConfig);
+  const arrayOfArrays = createTable(config);
+  arrayOfArrays.unshift(config.cols);
 
-export function exportXlsx(config: Readonly<ExportConfig>): IFile {
-  const { rows, cols, name } = parseExportConfig(config);
-  const workbook = XLSX.utils.book_new();
-  const sheetName = name;
-  workbook.SheetNames.push(sheetName);
+  const docDefinition = {
+    header: config.name,
+    content: [{
+      layout: "lightHorizontalLines",
+      table: {
+        headerRows: 1,
+        widths: config.cols.map((_) => "auto"),
+        body: arrayOfArrays,
+      },
+    }]
+  };
 
-  const toArray = new Function(`row`, `return [ ${cols.map((_) => `row[${JSON.stringify(_)}]`).join(",")} ]`) as (row: any) => any[];
-  const arrayOfArrays = rows.map(toArray);
-  arrayOfArrays.unshift(cols);
-  workbook.Sheets[sheetName] = XLSX.utils.aoa_to_sheet(arrayOfArrays);
 
-  const data = XLSX.write(workbook, {
-    type: "array",
-    bookType: "xlsb",
-    compression: true,
-    sheet: sheetName,
-  });
+  const pdf = pdfMake.createPdf(docDefinition);
+  const data = await new Promise<ArrayBuffer>((resolve) => pdf.getBuffer(resolve));
 
   return {
-    type: `application/vnd.ms-excel.sheet.binary.macroEnabled.12`,
     data,
-    name: `${name}.xlsb`,
+    name: `${config.name || "export"}.pdf`,
+    type: "application/pdf",
   };
 }
 
+export function exportXlsx(inputConfig: Readonly<ExportConfig>): IFile {
+  const config = parseExportConfig(inputConfig);
+  const workbook = createWorkbook(config);
+
+  const data = XLSX.write(workbook, {
+    type: "array",
+    bookType: "biff2",
+    sheet: workbook.SheetNames[0],
+  });
+
+  return {
+    type: `application/vnd.ms-excel`,
+    data,
+    name: `${config.name || "export"}.xls`,
+  };
+}
+
+function createTable(config: IExportConfig): any[][] {
+  const { rows, cols } = config;
+  const toArray = new Function(`row`, `return [ ${cols.map((_) => `row[${JSON.stringify(_)}]`).join(",")} ]`) as (row: any) => any[];
+  return rows.map(toArray);
+}
+
+function createWorkbook(config: IExportConfig): XLSX.WorkBook {
+  const workbook = XLSX.utils.book_new();
+  const sheetName = config.name;
+  workbook.SheetNames.push(sheetName);
+
+  const arrayOfArrays = createTable(config);
+  arrayOfArrays.unshift(config.cols);
+  workbook.Sheets[sheetName] = XLSX.utils.aoa_to_sheet(arrayOfArrays);
+
+  return workbook;
+}
+
 function parseExportConfig(config: Readonly<ExportConfig>): IExportConfig {
-  const name = "name" in config && config.name ? config.name : "Export";
+  const name = "name" in config && config.name ? config.name : "";
   const rows = config instanceof Array ? config.slice() : config.rows.slice();
   const cols = "rows" in config && config.rows ? config.rows : inferColsInRows(rows);
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
-import { exportXlsx } from "../src";
+import { exportXlsx, exportPdf } from "../src";
 import * as XLSX from "xlsx";
-import fs from "fs";
+import * as fs from "fs";
 
 const rows = [{
   id: 1,
@@ -15,9 +15,10 @@ test("exportXlsx() creates XLSX file", () => {
   const workbook = XLSX.read(blob.data, { type: "array" });
   const data = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
   expect(data).toEqual(rows);
+  fs.writeFileSync(__dirname + "/../sample/" + blob.name, Buffer.from(blob.data));
 });
 
-test("exportPdf() creates PDF file", () => {
-  const blob = exportPdf(rows);
-  fs.writeFileSync("../sample/export.pdf", blob.data);
+test("exportPdf() creates PDF file", async () => {
+  const blob = await exportPdf(rows);
+  fs.writeFileSync(__dirname + "/../sample/" + blob.name, Buffer.from(blob.data));
 });
